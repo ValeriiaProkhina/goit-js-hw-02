39b7f81dacc42fb0011e04cee5b4546d9e8f35e7
ValeriiaProkhina <140752336+ValeriiaProkhina@users.noreply.github.com>
2023-10-19 23:28:44 +0300

    add hw2-js

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,16 @@
-"use strict";
-function getElementWidth(content, padding, border) {
-  const elementWidth =
-    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-  return elementWidth;
+'use strict';
+function checkForSpam(message) {
+  const substringSpam = 'spam';
+  const substringSale = 'sale';
+  const result =
+    message.toLowerCase().includes(substringSpam) ||
+    message.toLowerCase().includes(substringSale);
+  return result;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
